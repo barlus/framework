baf618197058e29a8a4661bc207da1979b32562c
Sergey Mamyan <sergey.mamyan@gmail.com>
2017-12-28 01:44:18 +0400

    Initial Release

--- tool/publish.ts
+++ tool/publish.ts
@@ -1,5 +1,5 @@
 import "@barlus/node";
-import {readdirSync, realpathSync, statSync, writeFileSync} from "@barlus/node/fs";
+import {readdirSync, realpathSync, statSync, unlinkSync, writeFileSync} from "@barlus/node/fs";
 import {basename, dirname, resolve} from "@barlus/node/path";
 import {process} from '@barlus/node/process';
 import {execSync} from '@barlus/node/child_process';
@@ -35,12 +35,11 @@ projects.forEach(p => {
     if (!p.private) {
         console.info(p.name, p.version);
         const dir = dirname(p.filename);
-        writeFileSync(resolve(dir, '.npmrc'), [
-            'trict-ssl=false',
-            '//npm.sites.li/:_password=c2VyZ2V5',
-            '//npm.sites.li/:username=sergey',
-            '//npm.sites.li/:email=sergey@mamble.co',
-            '//npm.sites.li/:always-auth=false',
+        const rcf = resolve(dir, '.npmrc');
+        writeFileSync(rcf, [
+            `strict-ssl=false`,
+            `registry=https://packs.sites.li`,
+            `//packs.sites.li/:_authToken="bWMDpFkEnEiFJNfJqqF4R6BIpAsyy1d0aCq31wjlsy4="`,
             '',
         ].join('\n'));
         const cmd = `npm version patch && npm publish`;
@@ -55,5 +54,7 @@ projects.forEach(p => {
             console.error(ex);
             process.exit();
         }
+        unlinkSync(rcf)
     }
+    return true;
 });

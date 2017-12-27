1f645682a95b266114005f51327b6c424c352eb6
Sergey Mamyan <sergey.mamyan@gmail.com>
2017-12-27 21:32:05 +0400

    Initial Commit

--- tool/hello.ts
+++ tool/hello.ts
@@ -3,5 +3,5 @@ import {process} from "@barlus/node/process";
 declare const ts;
 
 console.info(`Hello From`);
-console.info(`  -> Node: ${process.version}`);
-console.info(`  -> TypeScript: ${ts.version}`);
+console.info(`  -> Node: ${process.version} ${process.cwd()}`);
+console.info(`  -> TypeScript: ${ts.version} ${ts.path}`);

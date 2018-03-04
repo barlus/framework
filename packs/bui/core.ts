d717aabbec9078332930c0d931feb2a596d6a4a0
vahrambleyan <vahram.bleyan@gmail.com>
2018-03-04 14:38:30 +0400

    changed style annotation behavior to not rewrite render method, instead added classNames properties to hold list of css class names

--- packs/bui/core.ts
+++ packs/bui/core.ts
@@ -5,9 +5,6 @@ import {injectable} from '@barlus/runtime/inject/decorators';
 import {TypeStyle, ReactComponent} from '@barlus/bui';
 import {NestedCSSProperties} from './typing/css';
 
-// new TypeStyle({
-//     autoGenerateTag: true
-// });
 class CSSHelper {
     static extend(source:NestedCSSProperties,target:NestedCSSProperties){
         for ( let key in target ) {
@@ -44,8 +41,34 @@ export function style(css: NestedCSSProperties|NestedCSSProperties[]) {
         decorator.style(target,css);
     }
 }
-export class Component<T,S={}> extends ReactComponent<T,S> {
-    classes:string[] = [];
+export class Component<T,S = {}> extends ReactComponent<T,S> {
+    get classNames():Array<string>{
+        return Object.defineProperty(this,'classNames',{
+            value:function(thiz):Array<string>{
+                let classes:Array<string> = [];
+                let parent = Object.getPrototypeOf(thiz);
+                while (parent instanceof Component) {
+                    if ( Reflect.hasOwnMetadata('component:style',parent.constructor) ) {
+                        let cname = parent.constructor.name;
+                        classes.push(cname);
+                    }
+                    parent = Object.getPrototypeOf(parent);
+
+                }
+
+                if( thiz.props.className ) {
+                    thiz.props.className.trim().split(/\s+/).forEach(c => {
+                        if (!classes.includes(c)) {
+                            classes.push(c);
+                        }
+                    });
+                }
+
+                return classes;
+            }(this),
+            writable:true
+        }).classNames;
+    }
 }
 
 @injectable
@@ -55,54 +78,16 @@ export class Decorator {
         this.ts = ts;
     }
     style(target:Function,css:NestedCSSProperties|NestedCSSProperties[]){
+        Reflect.defineMetadata('component:style',css,target);
         if (!(target.prototype instanceof Component)) {
             throw new Error('Elements must extends base class Element')
         }
         const className: string = target.name;
         const render = target.prototype.render;
-
-
         if (Array.isArray(css)){
             css = CSSHelper.merge(...css);
         }
-
         this.ts.cssRule(`.${className}`, css as NestedCSSProperties);
-        Object.defineProperty(target.prototype, 'render', {
-            value() {
-                let parent = target.prototype;
-                let classes = [];
-                while (parent instanceof Component) {
-                    let cname = parent.constructor.name;
-                    classes.push(cname);
-                    parent = Object.getPrototypeOf(parent);
-                }
-                const node: JSX.Element = render.apply(this, arguments);
-                if (typeof node.name != 'string') {
-                    console.info("Warning used on proxy")
-                }
-                if (!node.attributes) {
-                    Object.assign(node, {attributes: {}});
-                }
-                if (node.attributes.class) {
-                    node.attributes.class.trim().split(/\s+/).forEach(c => {
-                        if (!classes.includes(c)) {
-                            classes.push(c);
-                        }
-                    });
-                }
-                if(this.props.className){
-                    this.props.className.trim().split(/\s+/).forEach(c => {
-                        if (!classes.includes(c)) {
-                            classes.push(c);
-                        }
-                    });
-                    //classes = classes.concat(this.classes);
-                }
-
-                node.attributes.class = classes.join(' ');
-                return node;
-            }
-        })
     }
 }
 

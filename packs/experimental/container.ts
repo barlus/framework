f78b6c2fc4062201facf2c9c050a3cc724faeb30
Albert Mirzoyan <albert.mirzoyan@gmail.com>
2018-05-16 18:37:10 +0400

    experimaental async container utility

---
+++ packs/experimental/container.ts
@@ -0,0 +1,164 @@
+export class AsyncContainer<T> implements AsyncIterable<T>{
+    [Symbol.asyncIterator](): AsyncIterator<T> {
+        if (this.iteratable[Symbol.asyncIterator]) {
+            return this.iteratable[Symbol.asyncIterator]();
+        }
+        else if (this.iteratable[Symbol.iterator]) {
+            return this.iteratable[Symbol.iterator]();
+        }
+        else {
+            throw new Error("The child is not iteratable");
+        }
+    }
+    every(callbackfn: (value: T) => Promise<boolean>, thisArg?: any): Promise<boolean> {
+        return AsyncContainer.every(this[Symbol.asyncIterator](), callbackfn, thisArg);
+    }
+    some(callbackfn: (value: T) => Promise<boolean>, thisArg?: any): Promise<boolean> {
+        return AsyncContainer.some(this[Symbol.asyncIterator](), callbackfn, thisArg);
+    }
+    forEach(callbackfn: (value: T) => Promise<void | false>, thisArg?: any, brakeOnFalse: boolean = true): Promise<void> {
+        return AsyncContainer.forEach(this[Symbol.asyncIterator](), callbackfn, thisArg, brakeOnFalse);
+    }
+    reduce<U>(callbackfn: (previousValue: U, currentValue: T) => Promise<U>, initialValue: U, thisArg?: any): Promise<U> {
+        return AsyncContainer.reduce(this[Symbol.asyncIterator](), callbackfn, initialValue, thisArg);
+    }
+    map<U>(callbackfn: (value: T) => Promise<U>, thisArg?: any): AsyncContainer<U> {
+        return new AsyncContainer<U>({
+            [Symbol.asyncIterator]: (): AsyncIterator<U> => {
+                return AsyncContainer.map(this[Symbol.asyncIterator](), callbackfn, thisArg)
+            }
+        })
+    }
+    filter(callbackfn: (value: T) => Promise<boolean>, thisArg?: any): AsyncContainer<T> {
+        return new AsyncContainer<T>({
+            [Symbol.asyncIterator]: (): AsyncIterator<T> => {
+                return AsyncContainer.filter(this[Symbol.asyncIterator](), callbackfn, thisArg)
+            }
+        })
+    }
+    all(): Promise<T[]> {
+        return AsyncContainer.all(this[Symbol.asyncIterator]())
+    }
+
+    static async every<T>(iterator: AsyncIterator<T> | Iterator<T>, callbackfn: (value: T) => Promise<boolean>, thisArg?: any): Promise<boolean> {
+        let result = true;
+        await this.forEach(iterator, async v => {
+            if (thisArg) {
+                if (false === await callbackfn.call(thisArg, v)) {
+                    result = false;
+                    return false;
+                }
+            } else {
+                if (false === await callbackfn(v)) {
+                    result = false;
+                    return false;
+                }
+            }
+        })
+        return result;
+    }
+    static async some<T>(iterator: AsyncIterator<T> | Iterator<T>, callbackfn: (value: T) => Promise<boolean>, thisArg?: any): Promise<boolean> {
+        let result = false;
+        await this.forEach(iterator, async v => {
+            if (thisArg) {
+                if (true === await callbackfn.call(thisArg, v)) {
+                    result = true;
+                    return false;
+                }
+            } else {
+                if (true === await callbackfn(v)) {
+                    result = true;
+                    return false;
+                }
+            }
+        })
+        return result;
+    }
+    static async forEach<T>(iterator: AsyncIterator<T> | Iterator<T>, callbackfn: (value: T) => Promise<void | false>, thisArg?: any, brakeOnFalse: boolean = true): Promise<void> {
+        let result = await iterator.next();
+        while (!result.done) {
+            if (thisArg) {
+                if (false === await callbackfn.call(thisArg, result.value) && brakeOnFalse) {
+                    break;
+                }
+            } else {
+                if (false === await callbackfn(result.value) && brakeOnFalse) {
+                    break;
+                }
+            }
+            result = await iterator.next();
+        }
+    }
+    static async reduce<T, U>(iterator: AsyncIterator<T> | Iterator<T>, callbackfn: (previousValue: U, currentValue: T) => Promise<U>, initialValue: U, thisArg?: any): Promise<U> {
+        let aggregate = initialValue;
+        await this.forEach(iterator, async v => {
+            if (thisArg) {
+                aggregate = await callbackfn.call(thisArg, aggregate, v)
+            } else {
+                aggregate = await callbackfn(aggregate, v)
+            }
+        })
+        return aggregate;
+    }
+    static map<T, U>(iterator: AsyncIterator<T> | Iterator<T>, callbackfn: (value: T) => Promise<U>, thisArg?: any): AsyncIterator<U> {
+        return {
+            async next(value?: any): Promise<IteratorResult<U>> {
+                let result = await iterator.next(value);
+                if (result.done) {
+                    return {
+                        done: true,
+                        value: undefined
+                    }
+                }
+                let mapped: U;
+                if (thisArg) {
+                    mapped = await callbackfn.call(thisArg, result.value);
+                } else {
+                    mapped = await callbackfn(result.value);
+                }
+                return {
+                    done: false,
+                    value: mapped
+                }
+            }
+        }
+    }
+    static filter<T>(iterator: AsyncIterator<T> | Iterator<T>, callbackfn: (value: T) => Promise<boolean>, thisArg?: any): AsyncIterator<T> {
+        return {
+            async next(value?: any): Promise<IteratorResult<T>> {
+                let result = await iterator.next(value);
+                while (!result.done) {
+                    let condition: boolean;
+                    if (thisArg) {
+                        condition = await callbackfn.call(thisArg, result.value);
+                    } else {
+                        condition = await callbackfn(result.value);
+                    }
+                    if (true === condition) {
+                        return {
+                            done: false,
+                            value: result.value
+                        }
+                    }
+                    result = await iterator.next(value);
+                }
+                return {
+                    done: true,
+                    value: undefined
+                }
+            }
+        }
+    }
+    static async all<T>(iterator: AsyncIterator<T> | Iterator<T>, ): Promise<T[]> {
+        let result: T[] = [];
+        await this.forEach(iterator, async v => { result.push(v) });
+        return result;
+    }
+
+    constructor(private iteratable: AsyncIterable<T> | Iterable<T>) {
+
+    }
+    static from<E>(iteratable: AsyncIterable<E> | Iterable<E>): AsyncContainer<E> {
+        return new AsyncContainer<E>(iteratable)
+    }
+}

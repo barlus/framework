107783f7a19fc30d234aa68cbb9be895c8da90b8
Sergey Mamyan <sergey.mamyan@gmail.com>
2018-05-30 18:45:29 +0400

    Ui Library Changes

--- packs/spectre/index.ts
+++ packs/spectre/index.ts
@@ -1060,7 +1060,7 @@ cssRule('.menu .menu-badge', {
     "float": "right",
     "padding": ".2rem 0"
 });
-cssRule('.menu .menu-badge .btn', {
+cssRule('.menu .menu-badge .Button', {
     "marginTop": "-.1rem"
 });
 cssRule('.modal', {

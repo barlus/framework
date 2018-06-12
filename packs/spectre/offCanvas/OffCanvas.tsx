7a5b400b532010d2b4f8c901ed60c37010ebd61e
Sergey Mamyan <sergey.mamyan@gmail.com>
2018-06-12 19:19:19 +0400

    React/Redux integration

--- packs/spectre/offCanvas/OffCanvas.tsx
+++ packs/spectre/offCanvas/OffCanvas.tsx
@@ -1,59 +1,61 @@
-import * as React from "@barlus/react";
-import { Theme } from './theme';
-import { classes } from '../utils/classes';
-import { OffCanvasOverlay } from "./OffCanvasOverlay";
+import * as React         from "@barlus/react";
+import {Theme}            from './theme';
+import {classes}          from '../utils/classes';
+import {OffCanvasOverlay} from "./OffCanvasOverlay";
+
 
 export class OffCanvas extends React.PureComponent<OffCanvasProps, { active: boolean }> {
 
-    static defaultProps = {
-        active: false
-    };
+  static defaultProps = {
+    active: false
+  };
 
-    constructor(props: OffCanvasProps, ctx) {
-        super(props, ctx);
-        const { active } = props;
-        this.state = { active }
+  constructor(props: OffCanvasProps, ctx) {
+    super(props, ctx);
+    const { active } = props;
+    this.state = { active }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { active } = nextProps;
+    if (active != this.state.active) {
+      this.setState({ active })
     }
+  }
 
-    componentWillReceiveProps(nextProps) {
-        const { active } = nextProps;
-        if (active != this.state.active) {
-            this.setState({ active })
-        }
-    }
+  onClose = (e) => {
+    this.props.onBgClick && this.props.onBgClick(e);
+    this.props.closeOnBgClick && this.setState({ active: false });
+  };
 
-    onClose = (e) => {
-        this.props.onBgClick && this.props.onBgClick(e);
-        this.props.closeOnBgClick && this.setState({ active: false });
-    };
-
-    render() {
-        const {
-            className,
-            // Styles.
-            children,
-            closeOnBgClick,
-            onBgClick,
-            showOnLarge,
-            active,
-            ...otherProps
-        } = this.props;
-        return (
-            <div {...otherProps} className={classes(Theme.offCanvas, {
-                [ Theme.offCanvasSidebarShow ]: showOnLarge,
-                [ Theme.active ]: this.state.active
-            }, className)}>
-                {children}
-                <OffCanvasOverlay onClick={this.onClose}/>
-            </div>
-        )
-    }
+  render() {
+    const {
+      closeOnBgClick,
+      onBgClick,
+      active,
+      //Used above
+      className,
+      // Styles.
+      children,
+      showOnLarge,
+      ...otherProps
+    } = this.props;
+    return (
+      <div {...otherProps} className={classes(Theme.offCanvas, {
+        [ Theme.offCanvasSidebarShow ]: showOnLarge,
+        [ Theme.active ]: this.state.active
+      }, className)}>
+        {children}
+        <OffCanvasOverlay onClick={this.onClose}/>
+      </div>
+    )
+  }
 }
 
 export interface OffCanvasProps {
-    className?: string,
-    showOnLarge?: boolean,
-    active?: boolean,
-    closeOnBgClick?: boolean,
-    onBgClick?: Function,
+  className?: string,
+  showOnLarge?: boolean,
+  active?: boolean,
+  closeOnBgClick?: boolean,
+  onBgClick?: Function,
 }
